5f57c01b70b77df974bde186104ccaacfb59aad7
Isaac <izacstars@gmail.com>
2023-05-26 14:20:55 +0300

    additional configuratios

--- src/tsc/About/ab-sections/gallery.ts
+++ src/tsc/About/ab-sections/gallery.ts
@@ -5,48 +5,61 @@ import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
 library.add(faCircleArrowLeft, faCircleArrowRight);
 dom.watch();
 
-import img23 from '../../../assets/images/img23.jpg';
-import img26 from '../../../assets/images/img26.jpg';
-import img20 from '../../../assets/images/img20.jpg';
-import img16 from '../../../assets/images/img16.jpg';
-import img18 from '../../../assets/images/img18.jpg';
-import img25 from '../../../assets/images/img25.jpg';
-import img15 from '../../../assets/images/img15.jpeg';
+import img23 from "../../../assets/images/img23.jpg";
+import img26 from "../../../assets/images/img26.jpg";
+import img20 from "../../../assets/images/img20.jpg";
+import img16 from "../../../assets/images/img16.jpg";
+import img18 from "../../../assets/images/img18.jpg";
+import img25 from "../../../assets/images/img25.jpg";
+import img15 from "../../../assets/images/img15.jpeg";
+import img28 from "../../../assets/images/img28.jpg";
+import img17 from "../../../assets/images/img17.jpg";
+import img29 from "../../../assets/images/img29.jpg";
+/*
+  FOR SWIPER
+  TO HANDLE COVER FLOW CAROUSEL
 
+*/
+//swiper files
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination, EffectCoverflow } from "swiper";
+
+//css files
+import "swiper/swiper-bundle.min.css";
+// import "swiper/swiper.min.css";
 
 let abTemplate_3 = document.createElement("template");
 abTemplate_3.innerHTML = `
 <h1>GALLERY</h1>
-<button class="previous" is="slide-previous"><i class="fa-solid fa-circle-arrow-left"></i></button>
-<button class="next" is="slide-next"><i class="fa-solid fa-circle-arrow-right"></i></button>
 <div class="slider">
-    <div id="slider--text-container">
-      <p>Feel free to join us as we worship and praise the Lord.</p><br>
-      <p>Never miss a session and always keep up to date with the NDC</p>
-      <br><br>
-      <a href="https://www.youtube.com/@ndejjecollective"><em>Subscribe to Our Youtube Channel</em></a>
-    </div>
+  <div class="swiper">
 
-    <div class="slide-container">
-        <div class="slides slide1"><img src=${img23} alt="image1" loading="lazy" class="slide--img"></div>
-        <div class="slides slide2"><img src=${img26} alt="image2" loading="lazy" class="slide--img"></div>
-        <div class="slides slide3"><img src=${img20} alt="image3" loading="lazy" class="slide--img"></div>
-        <div class="slides slide4"><img src=${img18} alt="image4" loading="lazy" class="slide--img"></div>
-        <div class="slides slide5"><img src=${img16} alt="image5" loading="lazy" class="slide--img"></div>
-    </div>
-    <div class="mini-slide-container">
-      <div class="mini-slides"><img src=${img23} alt="image1" loading="lazy" ></div>
-      <div class="mini-slides"><img src=${img26} alt="image2" loading="lazy" ></div>
-      <div class="mini-slides"><img src=${img20} alt="image3" loading="lazy" ></div>
-      <div class="mini-slides"><img src=${img18} alt="image4" loading="lazy" ></div>
-      <div class="mini-slides"><img src=${img16} alt="image5" loading="lazy" ></div>
-      <div class="mini-slides"><img src=${img25} alt="image6" loading="lazy" ></div>
-      <div class="mini-slides"><img src=${img15} alt="image7" loading="lazy" ></div>
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+        <!-- Slides -->
+        <div class="swiper-slide"><img src=${img23} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img26} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img20} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img18} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img16} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img25} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img15} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img17} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img28} alt="image1" loading="lazy" ></div>
+        <div class="swiper-slide"><img src=${img29} alt="image1" loading="lazy" ></div>
 
     </div>
+
+    <!-- If we need pagination -->
+    <div class="swiper-pagination"></div>
+
+    <!-- If we need navigation buttons -->
+    <div class="swiper-button-prev swiper-btn"></div>
+    <div class="swiper-button-next swiper-btn"></div>
+
+  </div>
 </div>
-</div>
-
 `;
 
 export class Gallery extends HTMLElement {
@@ -56,118 +69,40 @@ export class Gallery extends HTMLElement {
 
   connectedCallback() {
     this.append(abTemplate_3.content.cloneNode(true));
+
+    this.swiperSlider();
   }
 
-  slideControl() {
-    var slideIndex = 1;
+  swiperSlider() {
+    new Swiper(".swiper", {
+      modules: [Navigation, Pagination, EffectCoverflow],
 
-    function slideShow(slide: number) {
-      let slidesNo: any = document.querySelectorAll(".slide--img");
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      loop: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
+      },
 
-      if (slide > slidesNo.length) {
-        slideIndex = 1;
-      }
-      if (slide < 1) {
-        slideIndex = slidesNo.length;
-      }
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination",
+      },
 
-      for (let i = 0; i < slidesNo.length; i++) {
-        slidesNo[i].style.display = "none";
-      }
-    }
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+    console.log("working");
   }
-
 }
 customElements.define("ab-gallery", Gallery);
 
-//previous slide functionality
-export class SlidePrevious extends HTMLButtonElement {
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
-    this.previousFunction();
-  }
-
-  previousFunction() {
-    var slideIndex = 1;
-    this.addEventListener("click", () => {
-      slideShow((slideIndex += -1));
-      let txt: any = document.getElementById('slider--text-container');
-      txt.style.display = 'block';
-      txt.style.width = '40%';
-      txt.style.height = '100%';
-    });
-
-    function slideShow(slide: number) {
-      let slidesNo: any = document.querySelectorAll(".slide--img");
-
-      if (slide > slidesNo.length) {
-        slideIndex = 1;
-      }
-      if (slide < 1) {
-        slideIndex = slidesNo.length;
-      }
-
-      for (let i = 0; i < slidesNo.length; i++) {
-        slidesNo[i].style.display = "none";
-      }
-
-      slidesNo[slideIndex - 1].style.display = "block";
-      slidesNo[slideIndex - 1].style.height = 600 + "px";
-      slidesNo[slideIndex - 1].style.width = 800 + "px";
-      slidesNo[slideIndex - 1].style.position = "static";
-
-    }
-  }
-}
-customElements.define("slide-previous", SlidePrevious, {
-  extends: "button",
-});
-
-//next slide functionality
-export class SlideNext extends HTMLButtonElement {
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
-    this.previousFunction();
-  }
-
-  previousFunction() {
-    var slideIndex = 1;
-    this.addEventListener("click", () => {
-      slideShow((slideIndex += 1));
-      let txt: any = document.getElementById('slider--text-container');
-      txt.style.display = 'block';
-      txt.style.width = '40%';
-      txt.style.height = '100%';
-    });
-
-    function slideShow(slide: number) {
-      let slidesNo: any = document.querySelectorAll(".slide--img");
-
-      if (slide > slidesNo.length) {
-        slideIndex = 1;
-      }
-      if (slide < 1) {
-        slideIndex = slidesNo.length;
-      }
-
-      for (let i = 0; i < slidesNo.length; i++) {
-        slidesNo[i].style.display = "none";
-      }
-
-      slidesNo[slideIndex - 1].style.display = "block";
-      slidesNo[slideIndex - 1].style.height = 600 + "px";
-      slidesNo[slideIndex - 1].style.width = 800 + "px";
-      slidesNo[slideIndex - 1].style.position = "static";
-
-    }
-  }
-}
-customElements.define("slide-next", SlideNext, {
-  extends: "button",
-});
